000b983b7def75714ffe62f2129c3cb7a946a4e5
Renan Martineli de Paula <renanmartineli@hotmail.com>
2020-04-28 06:42:20 -0300

    Updates

---
+++ precache-manifest.7ffdb4f06b811be3e1b368defa54399e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18fe0e7a3bf3ac3d425993ac0a676515",
+    "url": "/mern-chat/index.html"
+  },
+  {
+    "revision": "d267e97cf4b61e66b605",
+    "url": "/mern-chat/static/css/2.6e702559.chunk.css"
+  },
+  {
+    "revision": "70499d6dfe9269981229",
+    "url": "/mern-chat/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d267e97cf4b61e66b605",
+    "url": "/mern-chat/static/js/2.35a973ef.chunk.js"
+  },
+  {
+    "revision": "a2252ab558c25643e808ebcb56834eb1",
+    "url": "/mern-chat/static/js/2.35a973ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70499d6dfe9269981229",
+    "url": "/mern-chat/static/js/main.5fcf9511.chunk.js"
+  },
+  {
+    "revision": "d5132e39b866391cf3d2",
+    "url": "/mern-chat/static/js/runtime-main.1f8ae463.js"
+  }
+]);
